75aaf55a8a24744a4eeda0500567cfce9da14967
Hammad <127682041+Hammad005@users.noreply.github.com>
2026-03-17 22:05:04 +0500

    feat: add nodemailer for email functionality and update package.json dependencies

---
+++ server/model/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isVerfied: {
+        type: Boolean,
+        default: false
+    },
+    verificationOTP: String,
+    verificationOTPExpiry: Date,  
+    forgotPasswordOTP: String,
+    forgotPasswordOTPExpiry: Date
+}, {timestamps: true});
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
